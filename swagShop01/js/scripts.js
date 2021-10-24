4c533554144a496e0d5499f030a16dabd354409d
Kayla Macias <anahirivera99@live.com>
2021-10-23 23:17:49 -0600

    swag shop fixes

--- swagShop01/js/scripts.js
+++ swagShop01/js/scripts.js
@@ -1,8 +1,7 @@
-//Create a button and give it an ID of "hamburgerBtn"
-//Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
-function toggleMenu() {
+function toggleMenu(){
     document.getElementById("hamburgerBtn").classList.toggle("open");
     document.getElementById("primaryNav").classList.toggle("open");
 }
+
 var x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu; 
+x.onclick = toggleMenu;
